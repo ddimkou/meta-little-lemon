3b0b50fd2381ec8b4e2eac9eb12f814da65ffdfb
ddimkou <ddimkou@outlook.com>
2023-12-02 11:07:13 +0200

    hamburger menu

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./assets/images/Logo.svg";
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+    console.log(menuVisible);
+  };
   return (
     <header>
       <Link to="/">
         <img src={Logo} className="logo" alt="Little Lemon logo" />
       </Link>
-      <nav>
+      <nav className={menuVisible ? "menu-visible" : ""}>
         <ul>
           <li>
-            <Link to="/">Main</Link>
+            <Link to="/" onClick={toggleMenu}>
+              Main
+            </Link>
           </li>
           <li>
-            <Link to="/menu">Menu</Link>
+            <Link to="/menu" onClick={toggleMenu}>
+              Menu
+            </Link>
           </li>
           <li>
-            <Link to="/reserve">Book a Table</Link>
+            <Link to="/reserve" onClick={toggleMenu}>
+              Book a Table
+            </Link>
           </li>
         </ul>
       </nav>
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </div>
     </header>
   );
 };
