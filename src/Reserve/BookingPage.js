f5c0e071700619f684afa6e3d6db4508e3381f3a
ddimkou <ddimkou@outlook.com>
2023-12-08 13:08:36 +0200

    api setting

--- src/Reserve/BookingPage.js
+++ src/Reserve/BookingPage.js
@@ -1,14 +1,33 @@
+import { useEffect } from "react";
 import BookingForm from "./BookingForm";
 import Map from "./Map";
 import "./Reserve.css";
 import RestaurantInfos from "./RestaurantInfos";
 
-const BookingPage = ({ availableTimes, dispatch }) => {
+// Exercise: API request available hours but...
+// API server is not working 404.
+// avoiding the error with empty fetchAPI since it doesnt return
+const fetchAPI = () => {};
+
+const BookingPage = ({ availableTimes, updateTimes, dispatch }) => {
+  useEffect(() => {
+    const fetchAvailableTimes = async (date) => {
+      try {
+        const times = await fetchAPI(date.toISOString().split("T")[0]);
+        updateTimes(times);
+      } catch (error) {
+        console.error("Error fetching available times:", error);
+      }
+    };
+
+    const currentDate = new Date();
+    fetchAvailableTimes(currentDate);
+  }, [updateTimes]);
+
   return (
     <main className="reserve">
       <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
       <Map />
-
       <RestaurantInfos />
     </main>
   );
